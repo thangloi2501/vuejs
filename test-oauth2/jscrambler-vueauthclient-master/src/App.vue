c15620fad5935898e40b5cf9fd9d6d701c99623f
Loi Nguyen <thangloi2501@gmail.com>
2020-05-28 23:15:55 +0800

    aaa

---
+++ test-oauth2/jscrambler-vueauthclient-master/src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <div>
+      <router-link :to="{ name: 'Dashboard'}">Dashboard</router-link>
+      <router-link :to="{ name: 'Login'}">Login</router-link>
+      <a href="#" v-on:click="logout">Logout</a>
+    </div>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App",
+  methods: {
+    logout: function (e) {
+      axios.get("/api/logout")
+        .then(() => {
+          router.push("/")
+        })
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
